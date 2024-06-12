731a94e7ce9953230128e01232716b3c125522a7
Piyush Vishwakarma <piyushvishwakarma6706@gmail.com>
2024-06-12 13:03:46 +0530

    firstCommit

---
+++ Practice1.java
@@ -0,0 +1,76 @@
+
+import java.util.Arrays;
+
+public class Practice1 {
+
+
+//    //Q1. create an array of 5 floats and calculate their sum
+//    public static void main(String[] args) {
+//        float [] arr1= {2.3f,2.3f,4.2f,1.5f,6.3f};
+//        float sum=0;
+//        for (float v : arr1) {
+//            sum += v;
+//        }
+//        System.out.println(sum);
+//    }
+
+
+//
+
+    // Q2. create program to add two matrices of size 2x3
+//    public static void main(String[] args) {
+//        int [][] arr1= new int[2][3];
+//        arr1 [0][0]= 100;
+//        arr1 [0][1]= 100;
+//        arr1 [0][2]= 100;
+//        arr1 [1][0]= 100;
+//        arr1 [1][1]= 100;
+//        arr1 [1][2]= 100;
+//        int [][] arr2= new int[2][3];
+//        arr2 [0][0]= 100;
+//        arr2 [0][1]= 100;
+//        arr2 [0][2]= 100;
+//        arr2 [1][0]= 100;
+//        arr2 [1][1]= 100;
+//        arr2 [1][2]= 100;
+//        int [][] sum= new int[2][3];
+//
+//        for (int i = 0; i < sum.length; i++) {
+//            for(int j=0; j< sum[i].length; j++){
+//                sum[i][j]= arr1[i][j]+arr2[i][j];
+//            }
+//        }
+//        for (int i = 0; i < sum.length; i++) {
+//            for(int j=0; j< sum[i].length; j++){
+//                System.out.print(sum[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+    //Q3. write java program to reverse an array.
+//    public static void main(String[] args) {
+//        int [] arr= {1,2,3,4,5,6,7};
+//        int n= arr.length;
+//        for (int i = 0; i < n/2; i++) {
+//            int temp = arr[i];
+//            arr[i]=arr[n-i-1];
+//            arr[n-i-1]= temp;
+//        }
+//        for( int i : arr){
+//            System.out.print(i+",");
+//        }
+//    }
+    //Q4. write program to find max element in array.
+//    public static void main(String[] args) {
+//        int [] arr= {12,3,11,22,35,1,2,31,32,23};
+//        int max=arr[0];
+//        for (int i: arr){
+//            if(i<max){
+//                max=i;
+//            }
+//        }
+//        Arrays.sort(arr);
+//        System.out.println(arr[arr.length-1]);
+//    }
+
+}
